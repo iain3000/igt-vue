55bc753b7bcf7356dd2f75d3e11c099aad81a5cd
Iain Broadfoot <ibroadfo@icloud.com>
2023-06-25 14:06:22 +0100

    drop compat

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
-import vue from '@vitejs/plugin-vue'
+import createVuePlugin from '@vitejs/plugin-vue'
+
 const path = require("path");
 
 export default {
     resolve: {
       alias: {
-        vue: '@vue/compat',
         "@": path.resolve(__dirname, "./src"),
       }
     },
-    plugins: [
-      vue({
-        template: {
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-    ]
+    plugins: [ createVuePlugin() ]
   }
