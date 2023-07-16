8e80172f7235e27f501a4e425d4d694a541f6eda
Iain Broadfoot <ibroadfo@icloud.com>
2023-07-16 16:18:35 +0100

    use subscribe instead of onaction

--- tests/igt-library/tests/unit/ig-template/features/achievements/Achievements.spec.ts
+++ tests/igt-library/tests/unit/ig-template/features/achievements/Achievements.spec.ts
@@ -48,17 +48,9 @@ describe('Achievements', () => {
             )
         );
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        achievements.store.$onAction(({ name, store, args, after, onError }) => {
-            after((result) => {
-              expect(result.key).toBe(id);
-            })
-            onError((error) => {
-                console.error(error);
-                throw(error);
-            })
+        achievements.store.$subscribe((mutation, state) => {
+            expect(state.achievements[id].unlocked).toBeTruthy();
         })
-
         
         // Simulate 3 seconds twice to check for completion once
         achievements.update(3)
